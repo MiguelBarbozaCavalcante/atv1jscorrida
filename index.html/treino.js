877830b99271ac3f554bceafc2706a8185c2dae4
MiguelBarbozaCavalcante <105086306+MiguelBarbozaCavalcante@users.noreply.github.com>
2023-11-02 17:42:38 -0300

    Add files via upload

---
+++ index.html/treino.js
@@ -0,0 +1,40 @@
+const nome = "Miguel";
+
+
+function getEventoAleatorio() {
+  const numeroAleatorio = Math.floor(Math.random() * 3);
+  if (numeroAleatorio === 0) {
+    return "Maratona";
+  } else if (numeroAleatorio === 1) {
+    return "Triathlon";
+  } else {
+    return "Pentathlon";
+  }
+}
+
+
+function getDiasTreino(evento) {
+  if (evento === "Maratona") {
+    return 50;
+  } else if (evento === "Triathlon") {
+    return 100;
+  } else {
+    return 200;
+  }
+}
+
+
+function logEvento(nome, evento) {
+  console.log(`${nome} vai participar do evento: ${evento}`);
+}
+
+function logTempo(nome, dias) {
+  console.log(`${nome} vai treinar por ${dias} dias.`);
+}
+
+
+const eventoEscolhido = getEventoAleatorio();
+const diasDeTreino = getDiasTreino(eventoEscolhido);
+
+logEvento(nome, eventoEscolhido);
+logTempo(nome, diasDeTreino);
